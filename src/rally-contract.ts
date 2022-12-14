a55c06d2dc41982f1983f8650f2707d2e5b70b39
timotejGerzelj <timotej.gerzelj@gmail.com>
2022-12-14 14:39:56 +0100

    initial commit

--- src/rally-contract.ts
+++ src/rally-contract.ts
@@ -1,12 +1,14 @@
-import { ipfs, json, JSONValue, Value, store} from "@graphprotocol/graph-ts";
+import { ipfs, json, JSONValue, Value, store, Bytes} from "@graphprotocol/graph-ts";
 import {
   handleNewAudioChat as handleNewAudioChatEvent,
   handleAudioChatUpdated as handleAudioChatUpdated,
   handleAudioChatChangedState as handleAudioChatChangedStates,
-  handleAudioChatDeleted as handleAudioChatDeleted
+  handleAudioChatDeleted as handleAudioChatDeleted,
+  handleUpdateMetadataCID
 } from "../generated/RallyContract/RallyContract"
 import {
   AudioChat,
+  AudioChatMetadata
 } from "../generated/schema"
 import { integer } from "@protofire/subgraph-toolkit";
 
@@ -46,7 +48,18 @@ export function handlehandleNewAudioChat(event: handleNewAudioChatEvent): void {
     newAudioChat.state = event.params.current_state
     newAudioChat.is_indexed = event.params.is_indexed
     newAudioChat.creator = event.params.creator
-    let metadata = ipfs.cat(event.params.cid_metadata + "/data.json");
+
+    newAudioChat.save();      
+
+    }
+    }
+    
+
+  }
+
+ function handleMetadata(m: string, id: Bytes){
+  let newMetadata = new AudioChatMetadata(id.toHex());
+  let metadata = ipfs.cat(m + "/data.json");
     if (metadata) {
       const value = json.fromBytes(metadata).toObject();
       if (value){
@@ -63,79 +76,73 @@ export function handlehandleNewAudioChat(event: handleNewAudioChatEvent): void {
         let is_gated = value.get("is_gated")
         let max_attendees = value.get("max_attendees");
         if (name) {
-          newAudioChat.name = name.toString();
+          newMetadata.name = name.toString();
         }
         else {
-          newAudioChat.name = "";
+          newMetadata.name = "";
         }
         if(description){
-          newAudioChat.description = description.toString();
+          newMetadata.description = description.toString();
         }
         else {
-          newAudioChat.description = "";
+          newMetadata.description = "";
         }
         if (tags){
           let stringArray: JSONValue[] = tags.toArray() as Array<JSONValue>;
-          newAudioChat.tags = stringArray.map<string>((val: JSONValue): string => { return val.toString() }); 
+          newMetadata.tags = stringArray.map<string>((val: JSONValue): string => { return val.toString() }); 
         }
         else {
-          newAudioChat.tags = [];
+          newMetadata.tags = [];
         }
         if (image){
-          newAudioChat.image = image.toString();
+          newMetadata.image = image.toString();
         }
         else {
-          newAudioChat.image = "";
+          newMetadata.image = "";
         }
         if (has_cohosts){
-          newAudioChat.has_cohosts = has_cohosts.toBool();
+          newMetadata.has_cohosts = has_cohosts.toBool();
 
         }
         else {
-          newAudioChat.has_cohosts = false;
+          newMetadata.has_cohosts = false;
         }
         if (category) {
-          newAudioChat.category = category.toString()
+          newMetadata.category = category.toString()
         }
         else {
-          newAudioChat.category = "";
+          newMetadata.category = "";
         }
         if (is_nsfw){
-          newAudioChat.is_nsfw = is_nsfw.toBool();
+          newMetadata.is_nsfw = is_nsfw.toBool();
         }
         else {
-          newAudioChat.is_nsfw = false;
+          newMetadata.is_nsfw = false;
         }
         if (will_be_recorded){
-          newAudioChat.will_be_recorded = will_be_recorded.toBool()
+          newMetadata.will_be_recorded = will_be_recorded.toBool()
           
         }
         else {
-          newAudioChat.will_be_recorded = false
+          newMetadata.will_be_recorded = false
         }
         if (is_gated){
-          newAudioChat.is_gated = is_gated.toBool()
+          newMetadata.is_gated = is_gated.toBool()
         }
         else {
-          newAudioChat.is_gated = false;
+          newMetadata.is_gated = false;
         }
         if (max_attendees){
-          newAudioChat.max_attendees = max_attendees.toBigInt();
+          newMetadata.max_attendees = max_attendees.toBigInt();
           
         }
         else {
-          newAudioChat.max_attendees = integer.fromNumber(100);
+          newMetadata.max_attendees = integer.fromNumber(100);
         }
         
         }
       }
-      newAudioChat.save();      
-
-    }
-    }
-    
-
-  }
+ }
 
   export function handleEventUpdate(event: handleAudioChatUpdated): void {
       let id = event.params.audio_event_id;
@@ -157,88 +164,7 @@ export function handlehandleNewAudioChat(event: handleNewAudioChatEvent): void {
         if (loaded.cid_metadata != event.params.cid_metadata){
           loaded.cid_metadata = event.params.cid_metadata;
           let metadata = ipfs.cat(event.params.cid_metadata + "/data.json");
-    if (metadata) {
-      const value = json.fromBytes(metadata).toObject();
-      if (value){
-         let name = value.get("name");
-         let description = value.get("description")
-         let tags = value.get("tags")
-         let image = value.get("image")
-         let has_cohosts = value.get("has_cohosts")
-         //let cohosts_list = value.get("cohosts_list")
-         //let guests_list = value.get("guests_list")
-        let category = value.get("category")
-        let is_nsfw = value.get("is_nsfw")
-        let will_be_recorded = value.get("will_be_recorded")
-        let is_gated = value.get("is_gated")
-        let max_attendees = value.get("max_attendees");
-        if (name) {
-          loaded.name = name.toString();
-        }
-        else {
-          loaded.name = "";
-        }
-        if(description){
-          loaded.description = description.toString();
-        }
-        else {
-          loaded.description = "";
-        }
-        if (tags){
-          let stringArray: JSONValue[] = tags.toArray() as Array<JSONValue>;
-          loaded.tags = stringArray.map<string>((val: JSONValue): string => { return val.toString() }); 
-        }
-        else {
-          loaded.tags = [];
-        }
-        if (image){
-          loaded.image = image.toString();
-        }
-        else {
-          loaded.image = "";
-        }
-        if (has_cohosts){
-          loaded.has_cohosts = has_cohosts.toBool();
-
-        }
-        else {
-          loaded.has_cohosts = false;
-        }
-        if (category) {
-          loaded.category = category.toString()
-        }
-        else {
-          loaded.category = "";
-        }
-        if (is_nsfw){
-          loaded.is_nsfw = is_nsfw.toBool();
-        }
-        else {
-          loaded.is_nsfw = false;
-        }
-        if (will_be_recorded){
-          loaded.will_be_recorded = will_be_recorded.toBool()
-          
-        }
-        else {
-          loaded.will_be_recorded = false
-        }
-        if (is_gated){
-          loaded.is_gated = is_gated.toBool()
-        }
-        else {
-          loaded.is_gated = false;
-        }
-        if (max_attendees){
-          loaded.max_attendees = max_attendees.toBigInt();
-          
-        }
-        else {
-          loaded.max_attendees = integer.fromNumber(100);
-        }
-        }
-      }
-      
+   
     }
     loaded.save()
   } 
